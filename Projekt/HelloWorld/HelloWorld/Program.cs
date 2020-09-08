f3f7dde0b739e366a6b4e283129513b7b4df30cd
oggep <54796870+oggep@users.noreply.github.com>
2020-09-08 12:21:37 +0200

    Loop

--- Projekt/HelloWorld/HelloWorld/Program.cs
+++ Projekt/HelloWorld/HelloWorld/Program.cs
@@ -10,18 +10,47 @@ namespace HelloWorld
             string name = Console.ReadLine();
 
             Console.WriteLine("What is your age?");
-            var age = Console.ReadLine();
-
+            var age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Are you alive? Yes or no");
             var yesno = Console.ReadLine();
-
             Console.WriteLine(name + " " + age + " " + yesno);
 
+            if (18 <= age)
+            {
+                Console.WriteLine("Du är vuxen");
+            };
 
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + 1 + "Gruka");
+            }
+            Console.WriteLine("1, 2 or 3? Hitta korven");
+            var cases = Convert.ToInt32(Console.ReadLine());
+            switch (cases)
+            {
+                case 1:
+                    Console.WriteLine("Inte korv");
+                    break;
+                case 2:
+                    Console.WriteLine("korv");
+                    break;
+                case 3:
+                    Console.WriteLine("Inte korv");
+                    break;
+
+
+            }
+            Console.WriteLine("skriv siffran 1");
+            var k = Convert.ToInt32(Console.ReadLine());
+            while (k = 1)
+            {
+                Console.WriteLine("Grattis!");
+                k++;
+                break
+            };
 
         }
-
-    } 
+    }
 }
 
 
