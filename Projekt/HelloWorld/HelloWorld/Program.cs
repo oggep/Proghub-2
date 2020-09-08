ce11e138bb5353ac845f535a30145e41bdfadeb3
oggep <54796870+oggep@users.noreply.github.com>
2020-09-08 11:52:54 +0200

    hola

--- Projekt/HelloWorld/HelloWorld/Program.cs
+++ Projekt/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Are you alive? Yes or no");
+            var yesno = Console.ReadLine();
+
+            Console.WriteLine(name + " " + age + " " + yesno);
+
+
+
         }
-    }
+
+    } 
 }
+
+
